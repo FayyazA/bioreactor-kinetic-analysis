a4e9220143191a1e2931ddbab872b7199a6eaf04
Fayyaz Ahamed <fayyaz_ahamed@berkeley.edu>
2020-01-27 14:25:27 -0800

    Changed Input Function Definition

--- BioRx_3mets_Lac2pks_recover_old.m
+++ BioRx_3mets_Lac2pks_recover_old.m
@@ -49,10 +49,10 @@ function Mest_dyn = model_exchange_dyn(x)
 
     for k = 2:length(S_dyn)
        %gamma pdf approximation of input function , func = @(a,x)( a(3)*x.^(a(1)-1) .* exp(-x/a(2)) / (a(2)^a(1) * gamma(a(1))));
-       Inpfunc(:,k) =[ ( A(6)*(k*TR).^(A(4)-1) .* exp(-(k*TR)/A(5)) / (A(5)^A(4) * gamma(A(4))));...
-                        0;...
-                        0]; 
-       
+       %Inpfunc(:,k) =[ ( A(6)*(k*TR).^(A(4)-1) .* exp(-(k*TR)/A(5)) / (A(5)^A(4) * gamma(A(4))));...
+       %                 0;...
+       %                 0]; 
+       Inpfunc(1,k) = (Mest_dyn(1,k)-Mest_dyn(1,k-1)*exp((-x(1)-x(2)-x(3))*TR))* (x(1)+x(2)+x(3)) / (1-exp((-x(1)-x(2)-x(3))*TR));
        flip=30 ; % low flip angle (degree)
         
        Mest_dyn(:,k) = (K^-1)*Inpfunc(:,k-1) + expm(K*TR) * (Mest_dyn(:,k-1)*cos(flip*pi/180) - (K^-1)*Inpfunc(:,k-1));%Mest_dyn(:,k) = expm(K*TR)*Mest_dyn(:,k-1)*cos(flip*pi/180) +Inpfunc(:,k-1);
