f53b41d63ce42af9fc15e9fa8a2164059a777567
Fayyaz Ahamed <fayyaz_ahamed@berkeley.edu>
2020-02-05 13:46:02 -0800

    Changed the boundary condition for Lactate to 10 and added all experiments to the lactate sum code

--- run_multiple_files_Tissue_slices_lactate_sum.m
+++ run_multiple_files_Tissue_slices_lactate_sum.m
@@ -1,6 +1,6 @@
 function result = run_multiple_files_Tissue_slices_lactate_sum()
-theFiles = ["HK2_1_062714_LBp5","HK2_2_062714_LBp5","HK2B_1_062714_LBp5","UMRC6_1_C1Pyr_062714_LBp5","UMRC6_2_C1Pyr_062714_LBp5","UMRC6B_1_C1Pyr_062714_LBp5","UOK262_100813_1_LBp5","UOK262_100813B_1_LBp5","UOK262_042514_6_LBp5","NewUOKDataset","13C_3_uok262_101113","13C_1_C1Pyr_uok262_102213"];%["HK2_1_062714_LBp5","HK2_2_062714_LBp5","HK2B_1_062714_LBp5","UMRC6_1_C1Pyr_062714_LBp5","UMRC6_2_C1Pyr_062714_LBp5","UMRC6B_1_C1Pyr_062714_LBp5","UOK262_100813_1_LBp5","UOK262_100813B_1_LBp5","UOK262_042514_6_LBp5","NewUOKDataset"];
-starting_entries = [14,4,11,9,8,8,17,20,7,15,7,2];%[13,6,11,9,8,8,18,17,12,15];%did final starting point analysis with these values[13,5,11,9,6,11,17,16,12,16];%[13,5,3,9,2,8,13,11,19]; %[13,11,11,10,6,8,15,13,12]; %[13,11,11,12,6,11,15,13,12]
+theFiles = ["HK2_1_062714_LBp5","HK2_2_062714_LBp5","HK2B_1_062714_LBp5","UMRC6_1_C1Pyr_062714_LBp5","UMRC6_2_C1Pyr_062714_LBp5","UMRC6B_1_C1Pyr_062714_LBp5","UOK262_100813_1_LBp5","UOK262_100813B_1_LBp5","UOK262_042514_6_LBp5","NewUOKDataset","13C_3_uok262_101113_with_auto","13C_1_C1Pyr_uok262_102213_with_","UOK262_100813_5DiDS1mM_LBp5","UOK262B_100813_3_DiDS1mM_LBp5","UOK262_101113_8DiDS1mM_LBp5","UOK262_042514_7DiDS_LBp5"];
+starting_entries = [13,6,11,9,8,8,18,17,12,15,11,2,12,9,4,19]%[14,4,11,9,8,8,17,20,7,15,7,2,12,9,4,19];%[13,6,11,9,8,8,18,17,12,15,11,2];%did final starting point analysis with these values[13,5,11,9,6,11,17,16,12,16];%[13,5,3,9,2,8,13,11,19]; %[13,11,11,10,6,8,15,13,12]; %[13,11,11,12,6,11,15,13,12];
 pyr_table = zeros(10,5);
 lacin_table = zeros(12,10);
 lacex_table = zeros(10,18);
@@ -15,7 +15,7 @@ for k = 1 : length(theFiles)
     my_table = rows2vars(my_table);
     size_table = size(my_table);
     my_array = table2array(my_table(:,2:size_table(2)));
-    [X,S_fit_dyn,results] = BioRx_kinetics_Pyrfit_starting_out(input_vector,strcat(theFiles(k),"PyrTissuesmooth"));
+    [X,S_fit_dyn,results] = BioRx_kinetics_Pyrfit_starting_out_recover_old(input_vector,strcat(theFiles(k),"PyrTissuesmooth"));
     pyr_table(k,:) = results;
     %A_table(k,:) = round1_output;
     [C,S_fitdyn2] = Tissue_slices(my_array,X,strcat(theFiles(k),"LacTissue")); %[round2_output,fit2,results2] = BioRx_kinetics_Lacin_NoT1Lin(my_array,round1_output,strcat(theFiles(k),"Lacincs"));
@@ -33,8 +33,8 @@ for k = 1 : length(theFiles)
     %lacex_table(k,:) = E;
   drawnow; % Force display to update immediately.
 end
-xlswrite("Pyr_summarysupertightpyr_1_compartment.xlsx",pyr_table)
-xlswrite("LacTissue0215celldatasupertightpyr2_1_compartment.xlsx",lacin_table)
+xlswrite("Pyr_LooserBounds.xlsx",pyr_table)
+xlswrite("LacTissueLooserBounds.xlsx",lacin_table)
 %xlswrite("Lacex_summaryFINALsmooth.xlsx",lacex_table)
 %xlswrite("Atablescsmath",A_table)
 %xlswrite("Ctablescsmath",C_table)
