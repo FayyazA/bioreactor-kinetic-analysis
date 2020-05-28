74de2140f1c08cb097c4c3f259e8af122142d09f
Fayyaz Ahamed <fayyaz_ahamed@berkeley.edu>
2020-05-28 16:22:47 -0700

    Ran loops and wiped the spreadsheet to get starting points

--- run_multiple_files_modeling_RTSC_3compart.m
+++ run_multiple_files_modeling_RTSC_3compart.m
@@ -1,6 +1,6 @@
 function result = run_multiple_files_modeling_RTSC_3compart()
 theFiles = ["13C_1_RTSC_FP_CA_C1Pyr_032813"];
-starting_entries = [5,6,7,8,9,10,11,12,13,14,15];
+starting_entries = [6];
 %pyr_table = zeros(10,7);
 lacin_table = zeros(22,6);
 lacex_table = zeros(22,18);
@@ -18,10 +18,10 @@ previous_fitted_values = [1/46.334,0.0034343,0.12039,2.3562,21.803,2.55e+08;...
 lb = [min(previous_fitted_values(:,1)),min(previous_fitted_values(:,2)),min(previous_fitted_values(:,3)),min(previous_fitted_values(:,4)),min(previous_fitted_values(:,5)),min(previous_fitted_values(:,6))];
 ub = [max(previous_fitted_values(:,1)),max(previous_fitted_values(:,2)),max(previous_fitted_values(:,3)),max(previous_fitted_values(:,4)),max(previous_fitted_values(:,5)),max(previous_fitted_values(:,6))];
 AIC_vec = zeros(16,3);
-for k = 1 : length(starting_entries)
+for k = 1 : length(theFiles)
     data_set = importfilenew("C:\Users\Owner\Downloads\UOK262_UMRC6_HK2_BioRx_2peaks_control_data 04082020 with lactate added",theFiles(1),starting_entries(k))
     input_vector = data_set.Pyr_area';
-    [X,S_fit_dyn] = BioRx_kinetics_Pyrfit_starting_out_recover_old(input_vector,strcat(theFiles(1),"PyrRTSC_3comp"));
+    [X,S_fit_dyn] = BioRx_kinetics_Pyrfit_starting_out_recover_old(input_vector,strcat(theFiles(k),"PyrRTSC_3comp"));
     %pyr_table(k,:) = results1;
     %A_table(k,:) = round1_output;
     my_table = array2table(data_set{:,[4,8]});
@@ -30,7 +30,7 @@ for k = 1 : length(starting_entries)
     my_table = rows2vars(my_table);
     size_table = size(my_table);
     my_array = table2array(my_table(:,2:size_table(2)));
-    [C,S_fitdyn2] = BioRx_Pyr_Lacin_recover_old_T1Lin_19(my_array,X,strcat(theFiles(1),"LacinRTSC_3comp")); %[round2_output,fit2,results2] = BioRx_kinetics_Lacin_NoT1Lin(my_array,round1_output,strcat(theFiles(k),"Lacincs"));
+    [C,S_fitdyn2] = BioRx_Pyr_Lacin_recover_old_T1Lin_19(my_array,X,strcat(theFiles(k),"LacinRTSC_3comp")); %[round2_output,fit2,results2] = BioRx_kinetics_Lacin_NoT1Lin(my_array,round1_output,strcat(theFiles(k),"Lacincs"));
     lacin_table(k,:) = C;
     %C_table(k,:) = round2_output;
     my_table_2 = array2table(data_set{:,[4,8,10]});
@@ -41,7 +41,7 @@ for k = 1 : length(starting_entries)
     my_table_2 = rows2vars(my_table_2);
     size_table_2 = size(my_table_2)
     my_array_2 = table2array(my_table_2(:,2:size_table(2)));
-    [E,Sfitdyn3] = BioRx_3mets_Lac2pks_recover_old_T1Lin_19(my_array_2,X,C,strcat(theFiles(1),"LacexRTSC_3comp")); %[fit3,results3] = BioRx_kinetics_Lac2pks_NoT1s(my_array_2,round1_output,round2_output,strcat(theFiles(k),"Lacexcs"));
+    [E,Sfitdyn3] = BioRx_3mets_Lac2pks_recover_old_T1Lin_19(my_array_2,X,C,strcat(theFiles(k),"LacexRTSC_3comp")); %[fit3,results3] = BioRx_kinetics_Lac2pks_NoT1s(my_array_2,round1_output,round2_output,strcat(theFiles(k),"Lacexcs"));
     AIC = aic_3(my_array_2-Sfitdyn3,length(S_fit_dyn),9);
     AIC_vec(k,:) = AIC;
     lacex_table(k,:) = E;
